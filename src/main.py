0201ff5f1e1884bb532fdd788f0a244dd2cccc0c
Umulqayr Ayanle <uayanle@gmail.com>
2026-07-22 14:44:01 +0100

    Initial project structure

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+root = tk.Tk()  # creates the main application window
+
+# window title
+root.title("Electrical Engineering Calculator")
+
+# window size
+root.geometry("1000x600")
+
+# prevention of window becoming too small
+root.minsize(800, 500)
+
+# keep the application running
+root.mainloop()
